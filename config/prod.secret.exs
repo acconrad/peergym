5df3cde3e68444b84e9d23a1db6ee26157456669
Adam Conrad <adam@starstreet.com>
2015-07-10 03:36:55 -0400

    [ACC] Heroku deployment params

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,12 +4,10 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :peergym, Peergym.Endpoint,
-  secret_key_base: "kceYatgVZqioohOb3VkRFKn8+epAXW7PvmS85bdYrhHeyUP4eigdZD3txN3Ovoqe"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :peergym, Peergym.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "peergym_prod",
+  url: System.get_env("DATABASE_URL"),
   size: 20 # The amount of database connections in the pool
