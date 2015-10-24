01bb8030291e2352a9718ec871f303a3f399cc08
Adam Conrad <adam@starstreet.com>
2015-10-24 18:09:43 -0400

    [ACC] Fix gym search issue on homepage. Patch search box for mobile.

--- web/static/js/peergym.js
+++ web/static/js/peergym.js
@@ -55,26 +55,29 @@ export class Maps {
 
   initializeSearch(){
     var doc = document
-      , location = doc.getElementById( 'search_location' )
-      , form = doc.getElementById( 'gym-search-form' );
+      , locations = doc.querySelectorAll( '.form-gym-location' )
+      , forms = doc.querySelectorAll( '.form-gym-search' );
 
-    new google.maps.places.Autocomplete( location );
+    Array.prototype.forEach.call( locations, location => {
+      new google.maps.places.Autocomplete( location );
+    });
 
-    form.addEventListener( 'submit', event => {
-      event.preventDefault();
-
-      new google.maps.Geocoder().geocode( { 'address': location.value }, (results, status) => {
-        var result, latlng;
-        if ( status === google.maps.GeocoderStatus.OK ) {
-          result = results[0];
-          latlng = result.geometry.location.toString().slice(1,-1).split(', ');
-          document.getElementById( 'search_lat' ).value = latlng[0];
-          document.getElementById( 'search_lng' ).value = latlng[1];
-          document.getElementById( 'search_place' ).value = result.place_id;
-          form.submit();
-        } else {
-          alert( 'Geocode was not successful for the following reason: ' + status );
-        }
+    Array.prototype.forEach.call( forms, form => {
+      form.addEventListener( 'submit', event => {
+        event.preventDefault();
+        new google.maps.Geocoder().geocode( { 'address': form.querySelector('.form-gym-location').value }, (results, status) => {
+          var result, latlng;
+          if ( status === google.maps.GeocoderStatus.OK ) {
+            result = results[0];
+            latlng = result.geometry.location.toString().slice(1,-1).split(', ');
+            form.querySelector( '#search_lat' ).value = latlng[0];
+            form.querySelector( '#search_lng' ).value = latlng[1];
+            form.querySelector( '#search_place' ).value = result.place_id;
+            form.submit();
+          } else {
+            alert( 'Geocode was not successful for the following reason: ' + status );
+          }
+        });
       });
     });
   }
